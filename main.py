d8e98fb7cbfa9fd6c1695c2deee8c933c5294de5
07adii04 <adityatiwari0at.adi@gmail.com>
2026-06-30 15:55:29 +0530

    feat: done with fronten end added new features like ui , upload button,and tts with play ,pause and stop option

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from routers import ocr
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 app = FastAPI(title="Indic Lens V2")
 
 app.include_router(ocr.router, prefix="/api/v1")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def root():
-    return {"message": "Indic Lens V2 — check /docs"}
+    with open("static/index.html") as f:
+        return f.read()
